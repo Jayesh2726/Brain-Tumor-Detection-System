d2bd2197baaa985001bcb2bf9966d587e07da646
Jayesh Magare <141212191+Jayesh2726@users.noreply.github.com>
2025-12-16 17:02:33 +0530

    Change port number 8052

--- app.py
+++ app.py
@@ -143,7 +143,8 @@ def health_check():
 
 if __name__ == '__main__':
     # Get port from environment variable (Render provides this)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8052))
 
     app.run(debug=False, host='0.0.0.0', port=port)
+
 
