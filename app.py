345d84d949f27614c266c9f6ceff6e84aaa2483a
Jayesh Magare <141212191+Jayesh2726@users.noreply.github.com>
2025-12-16 16:56:01 +0530

    Fix another port from 10000 to 50000

--- app.py
+++ app.py
@@ -143,5 +143,6 @@ def health_check():
 
 if __name__ == '__main__':
     # Get port from environment variable (Render provides this)
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 50000))
+
     app.run(debug=False, host='0.0.0.0', port=port)
